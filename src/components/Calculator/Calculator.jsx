be0eaff670c2291ddcbee75626ff571a30681e53
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-04-13 19:58:10 +0200

    feat(us#1): add equals key to calculator

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,14 +1,40 @@
 import { Component } from 'react';
-import Key from '../Key/Key';
+import ResultKey from '../Key/ResultKey/ResultKey';
+import { RESULT } from '../../utils/constants';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      input: '',
+      display: '',
+    };
+    this.displayResult = this.displayResult.bind(this);
+  }
+
+  displayResult(result) {
+    this.setState(() => ({ display: result }));
   }
 
   render() {
-    return <Key id="one" value="1" handleClick={() => {}} />;
+    const { input, display } = this.state;
+    const {
+      style: resultStyle,
+      key: { id: resultId, value: resultValue },
+    } = RESULT;
+
+    return (
+      <div>
+        <ResultKey
+          input={input}
+          displayResult={this.displayResult}
+          id={resultId}
+          value={resultValue}
+          style={resultStyle}
+          key={resultId}
+        />
+      </div>
+    );
   }
 }
 
