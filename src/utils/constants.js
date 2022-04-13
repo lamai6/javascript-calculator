30b3fa7bc16510314d786dd45eea5a3780f5a0bf
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-04-13 18:37:17 +0200

    feat(utils): add all keys information

---
+++ src/utils/constants.js
@@ -0,0 +1,73 @@
+const NUMBERS = {
+  style: {
+    backgroundColor: '#4d4d4d',
+  },
+  keys: [
+    {
+      id: 'zero',
+      value: '0',
+    },
+    {
+      id: 'one',
+      value: '1',
+    },
+    {
+      id: 'two',
+      value: '2',
+    },
+    {
+      id: 'three',
+      value: '3',
+    },
+    {
+      id: 'four',
+      value: '4',
+    },
+    {
+      id: 'five',
+      value: '5',
+    },
+    {
+      id: 'six',
+      value: '6',
+    },
+    {
+      id: 'seven',
+      value: '7',
+    },
+    {
+      id: 'eight',
+      value: '8',
+    },
+    {
+      id: 'nine',
+      value: '9',
+    },
+  ],
+};
+
+const OPERATORS = {
+  style: {
+    backgroundColor: '#777',
+  },
+  keys: [
+    {
+      id: 'add',
+      value: '+',
+    },
+    {
+      id: 'subtract',
+      value: '-',
+    },
+    {
+      id: 'multiply',
+      value: '*',
+    },
+    {
+      id: 'divide',
+      value: '/',
+    },
+  ],
+};
+
+export { NUMBERS, OPERATORS };
