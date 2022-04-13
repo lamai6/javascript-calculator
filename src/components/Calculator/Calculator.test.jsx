740e056e9378bc504cb51101143fa3f37477c087
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-04-13 20:36:59 +0200

    test(tdd): add test for user story #3

--- src/components/Calculator/Calculator.test.jsx
+++ src/components/Calculator/Calculator.test.jsx
@@ -12,10 +12,8 @@ describe('Product Backlog test suite', () => {
 
   it('should contain 10 clickable elements containing one number each from 0-9 with their own number as an id (US#2)', () => {
     const { getAllByRole } = render(<Calculator />);
-    const equalsButton = getAllByRole('button');
-    const numeralButtons = equalsButton.filter(
-      (button) => button.innerHTML <= 9
-    );
+    const buttons = getAllByRole('button');
+    const numeralButtons = buttons.filter((button) => button.innerHTML <= 9);
     const ids = [
       'zero',
       'one',
@@ -34,5 +32,14 @@ describe('Product Backlog test suite', () => {
 
     expect(numeralButtons).toHaveLength(10);
     expect(isButtonsIdCorrect).toBe(true);
+  });
+
+  it('should contain 4 clickable elements each containing one of the 4 primary mathematical operators with their id (US#3)', () => {
+    const { getAllByRole } = render(<Calculator />);
+    const ids = ['add', 'subtract', 'multiply', 'divide'];
+    const buttons = getAllByRole('button');
+    const operatorButtons = buttons.filter((button) => ids.includes(button.id));
+
+    expect(operatorButtons).toHaveLength(4);
   });
 });
