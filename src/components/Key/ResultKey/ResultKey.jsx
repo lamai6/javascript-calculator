90e9e30a73cfb5b3e7560a0619b56e7dcacbdf0d
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-04-14 20:09:56 +0200

    fix(us#9): prevent adding unnecessary zeros to result with toFixed

--- src/components/Key/ResultKey/ResultKey.jsx
+++ src/components/Key/ResultKey/ResultKey.jsx
@@ -2,10 +2,14 @@ import PropTypes from 'prop-types';
 import Key from '../Key';
 
 function ResultKey({ id, value, style, input, setResult }) {
+  const formatDecimalValues = (result) => {
+    const decimal = `${result}`.split('.')[1];
+    if (decimal?.length > 4) return `${result.toFixed(4)}`;
+    return `${result}`;
+  };
+
   const handleClick = () => {
-    let result = eval(input);
-    if (`${result}`.includes('.')) result = result.toFixed(4);
-    setResult(`${result}`);
+    setResult(formatDecimalValues(eval(input)));
   };
 
   return <Key id={id} value={value} style={style} handleClick={handleClick} />;
