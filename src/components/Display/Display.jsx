9cba59c4aabf811d5e8f4bbec8a440303d36b7ea
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-04-17 15:00:24 +0200

    feat(style): style calculator display section

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -1,10 +1,17 @@
 import PropTypes from 'prop-types';
+import './Display.styles.scss';
 
-function Display({ result }) {
-  return <div id="display">{result}</div>;
+function Display({ input, result }) {
+  return (
+    <div id="display-container">
+      <div id="input">{input}</div>
+      <div id="display">{result}</div>
+    </div>
+  );
 }
 
 Display.propTypes = {
+  input: PropTypes.string.isRequired,
   result: PropTypes.string.isRequired,
 };
 
