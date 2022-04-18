c3204e43eed378eb8c0b3a56acdc012a4e0b6d13
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-04-18 18:15:27 +0200

    feat(fcc): hide fcc test suite on page load

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,7 @@ import './App.styles.scss';
 
 function App() {
   localStorage.setItem('project_selector', 'javascript-calculator');
-
+  localStorage.setItem('fCC_javascript-calculator_hide', 'true');
   return (
     <div id="container">
       <Calculator />
